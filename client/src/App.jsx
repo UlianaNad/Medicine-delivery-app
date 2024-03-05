620667ed9770339d556c4dfe628713d88d1d2304
”John <102276922+UlianaNad@users.noreply.github.com>
2024-03-05 19:34:46 +0200

    created routes

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+//import { lazy } from "react";
+import Shops from "./pages/Shops";
+import Cart from "./pages/Cart";
+
+//const HomePage = lazy(() => import("./pages/HomePage"));
+//const Cart = lazy(() => import("./pages/Cart"));
+//const Shops = lazy(() => import("./pages/Shops"));
+
+export const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route index element={<HomePage />} /> */}
+          <Route path="shops" element={<Shops />} />
+          <Route path="cart" element={<Cart />} />
+        </Route>
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+      <ToastContainer />
+      <GlobalStyle />
+    </div>
+  );
+};
